856c7492001baf51965ad0531b6891a9aca4447e
Juergen Enge <github@betalabs.ch>
2020-11-07 18:22:29 +0100

    mediaserver changed

--- pkg/zotmedia/mediaserver.go
+++
@@ -1,9 +0,0 @@
-package zotmedia
-
-type Mediaserver interface {
-	IsMediaserverURL(url string) (string, string, bool)
-	GetCollectionByName(name string) (*Collection, error)
-	GetCollectionById(id int64) (*Collection, error)
-	CreateMasterUrl(collection, signature, url string) error
-	GetMetadata(collection, signature string) (*Metadata, error)
-}
